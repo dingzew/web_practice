24116f007bfafca03dbc5da5870c8d8be4108fad
Dingze <dingzew@andrew.cmu.edu>
2018-06-28 23:36:11 -0400

    jacquard search page

---
+++ jacquard_django/drawingManageApp/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+# def loadDrawingRequest(request):
+#     drawings = Drawing.objects.all()
+#     result = ""
+#     for drawing in drawings:
+#         result = result + drawing.DrawingID + ","
+#         result = result + drawing.BuildingName + ","
+#         result = result + str(drawing.ConstructedYear) + ","
+#         result = result + drawing.Contractor + ","
+#         result = result + drawing.Floor + ","
+#         result = result + drawing.Shop + ";"
+#
+#     result = result[:-1]
+#
+#     return HttpResponse(result)
+#
+# def addDrawingRecord(request):
+#     try:
+#
+#         drawingID = request.GET["DrawingID"]
+#         BuildingName = request.GET["BuildingName"]
+#         ConstructedYear = request.GET["ConstructedYear"]
+#         Contractor = request.GET["Contractor"]
+#         Floor = request.GET["Floor"]
+#         Shop = request.GET["Shop"]
+#
+#         newDrawing = Drawing(DrawingID = drawingID,BuildingName=BuildingName,ConstructedYear=ConstructedYear,Contractor=Contractor,Floor=Floor,Shop=Shop)
+#         newDrawing.save()
+#         return HttpResponse("OK")
+#     except:
+#         return HttpResponse("Failed")
+#
+#
+# def updateDrawingRecord(request):
+#     try:
+#         drawingID = request.GET["DrawingID"]
+#         BuildingName = request.GET["BuildingName"]
+#         ConstructedYear = request.GET["ConstructedYear"]
+#         Contractor = request.GET["Contractor"]
+#         Floor = request.GET["Floor"]
+#         Shop = request.GET["Shop"]
+#
+#         drawingRecord = Drawing.objects.get(DrawingID=drawingID)
+#
+#         drawingRecord.BuildingName = BuildingName
+#         drawingRecord.ConstructedYear = ConstructedYear
+#         drawingRecord.Contractor = Contractor
+#         drawingRecord.Floor = Floor
+#         drawingRecord.Shop= Shop
+#
+#         drawingRecord.save()
+#
+#         return HttpResponse("OK")
+#     except:
+#         return HttpResponse("Failed")
+
---
+++ jacquard_django/static/toggle/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/daviddarnes/pen/LdGYVG.
+
+ Based on Toggle by Daryl Ginn https://dribbble.com/shots/4318035-Toggle
